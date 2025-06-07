b7a081c76ac7fb387715d22a1bffcab733829614
amolpawar77 <amolpawar7015@gmail.com>
2025-06-07 11:42:33 +0530

    latest changes

---
+++ README.md
@@ -0,0 +1,79 @@
+<<<<<<< HEAD
+# AG Grid Finance Demo (Angular)
+
+The [AG Grid Finance Demo](https://ag-grid.com/example-finance/) code in Angular.
+
+## Getting Started
+
+1. Get a copy of this folder using [degit](https://github.com/Rich-Harris/degit) (without the git respository files):
+
+   ```
+   npx degit ag-grid/ag-grid-demos/finance/angular ag-grid-finance-example-angular
+   cd ag-grid-finance-example-angular
+   ```
+
+   Alternatively, you can get the files using `git clone`:
+
+   ```
+   git clone git@github.com:ag-grid/ag-grid-demos.git
+   cd ag-grid-demos/finance/angular
+   ```
+
+2. Install dependencies: `npm install`
+3. Install the Angular CLI: `npm install -g @angular/cli`
+4. Run the dev server: `ng serve`
+
+## How It Was Built
+
+This example code was generated with the [Angular CLI](https://github.com/angular/angular-cli) using:
+
+```
+ng new finance/angular
+
+# With the addition of the following modules
+npm i @ag-grid-community/angular \
+  @ag-grid-community/styles \
+  @ag-grid-community/core \
+  @ag-grid-community/client-side-row-model \
+  @ag-grid-enterprise/advanced-filter \
+  @ag-grid-enterprise/charts-enterprise \
+  @ag-grid-enterprise/column-tool-panel \
+  @ag-grid-enterprise/excel-export \
+  @ag-grid-enterprise/filter-tool-panel \
+  @ag-grid-enterprise/menu \
+  @ag-grid-enterprise/range-selection \
+  @ag-grid-enterprise/rich-select \
+  @ag-grid-enterprise/row-grouping \
+  @ag-grid-enterprise/set-filter \
+  @ag-grid-enterprise/sparklines \
+  @ag-grid-enterprise/status-bar
+```
+
+<br /><br />
+
+## Support
+
+### Enterprise Support
+
+AG Grid Enterprise customers have access to dedicated support via [ZenDesk](https://ag-grid.zendesk.com/hc/en-us), which is monitored by our engineering teams.
+
+### Bug Reports
+
+If you have found a bug, please report it in our main repository's [issues](https://github.com/ag-grid/ag-grid/issues) section.
+=======
+# AG-Grid-Angular
+This project is a demo implementation of AG Grid using Angular. AG Grid is a powerful and feature-rich data grid library that provides functionalities like sorting, filtering, pagination, and more, making it ideal for enterprise-level applications
+The goal of this demo is to showcase how to integrate AG Grid into an Angular application, along with basic configurations and examples to get started. It includes:
+
+✅ Integration of AG Grid in Angular
+
+✅ Displaying static and dynamic data in grid
+
+✅ Sorting, filtering, and pagination features
+
+✅ Custom cell rendering
+
+✅ Responsive layout
+
+This project is suitable for developers who are new to AG Grid or want a quick reference for integrating it into Angular projects
+>>>>>>> fd47b40133fb3150fb35961ba007f618fcd5a7b0
---
+++ src/app/finance-example/finance-example.component.ts
@@ -0,0 +1,178 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+import {
+  type ColDef,
+  type GetRowIdFunc,
+  type GetRowIdParams,
+  type GridReadyEvent,
+  type ValueFormatterFunc,
+  type ValueGetterParams,
+} from '@ag-grid-community/core';
+import { AgGridAngular } from '@ag-grid-community/angular';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { AdvancedFilterModule } from '@ag-grid-enterprise/advanced-filter';
+import { GridChartsModule } from '@ag-grid-enterprise/charts-enterprise';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { RichSelectModule } from '@ag-grid-enterprise/rich-select';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
+import { SparklinesModule } from '@ag-grid-enterprise/sparklines';
+import { StatusBarModule } from '@ag-grid-enterprise/status-bar';
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-quartz.css';
+import { getData } from './data';
+
+import { TickerCellRenderer } from './cell-renderers/ticker-cell-renderer.component';
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  AdvancedFilterModule,
+  ColumnsToolPanelModule,
+  ExcelExportModule,
+  FiltersToolPanelModule,
+  GridChartsModule,
+  MenuModule,
+  RangeSelectionModule,
+  RowGroupingModule,
+  SetFilterModule,
+  RichSelectModule,
+  StatusBarModule,
+  SparklinesModule,
+]);
+
+const numberFormatter: ValueFormatterFunc = (params) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    maximumFractionDigits: 2,
+  });
+  return params.value == null ? '' : formatter.format(params.value);
+};
+
+@Component({
+  selector: 'finance-example',
+  standalone: true,
+  imports: [AgGridAngular, TickerCellRenderer],
+  templateUrl: './finance-example.component.html',
+  styleUrl: './finance-example.component.css',
+  encapsulation: ViewEncapsulation.None,
+})
+export class FinanceExample {
+  @Input() gridTheme: string = 'ag-theme-quartz';
+  @Input() isDarkMode: boolean = false;
+
+  themeClass = `${this.gridTheme}${this.isDarkMode ? '-dark' : ''}`;
+
+  rowData = getData();
+  getRowId: GetRowIdFunc = (params: GetRowIdParams) => params.data.ticker;
+  onGridReady(params: GridReadyEvent) {
+    setInterval(() => {
+      this.rowData = this.rowData.map((item) =>
+        Math.random() < 0.1
+          ? {
+            ...item,
+            price:
+              item.price +
+              item.price *
+              ((Math.random() * 4 + 1) / 100) *
+              (Math.random() > 0.5 ? 1 : -1),
+          }
+          : item,
+      );
+    }, 1000);
+  }
+  enableRangeSelection: boolean = true;
+  rowSelection: 'multiple' | 'single' | undefined = 'multiple';
+  enableCharts: boolean = true;
+  rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' | undefined =
+    'always';
+  suppressAggFuncInHeader: boolean = true;
+  groupDefaultExpanded = -1;
+
+  defaultColDef: ColDef = {
+    flex: 1,
+    filter: true,
+    enableRowGroup: true,
+    enableValue: true,
+  };
+  colDefs: ColDef[] = [
+    {
+      field: 'ticker',
+      cellRenderer: TickerCellRenderer,
+      minWidth: 380,
+    },
+    {
+      field: 'instrument',
+      cellDataType: 'text',
+      type: 'rightAligned',
+      maxWidth: 180,
+    },
+    {
+      headerName: 'P&L',
+      cellDataType: 'number',
+      type: 'rightAligned',
+      cellRenderer: 'agAnimateShowChangeCellRenderer',
+      valueGetter: ({ data }: ValueGetterParams) =>
+        data && data.quantity * (data.price / data.purchasePrice),
+      valueFormatter: numberFormatter,
+      aggFunc: 'sum',
+    },
+    {
+      headerName: 'Total Value',
+      type: 'rightAligned',
+      cellDataType: 'number',
+      valueGetter: ({ data }: ValueGetterParams) =>
+        data && data.quantity * data.price,
+      cellRenderer: 'agAnimateShowChangeCellRenderer',
+      valueFormatter: numberFormatter,
+      aggFunc: 'sum',
+    },
+    {
+      field: 'quantity',
+      cellDataType: 'number',
+      type: 'rightAligned',
+      valueFormatter: numberFormatter,
+      maxWidth: 150,
+    },
+    {
+      headerName: 'Price',
+      field: 'purchasePrice',
+      cellDataType: 'number',
+      type: 'rightAligned',
+      valueFormatter: numberFormatter,
+      maxWidth: 150,
+    },
+    {
+      field: 'purchaseDate',
+      cellDataType: 'dateString',
+      type: 'rightAligned',
+      hide: true,
+    },
+    {
+      headerName: 'Last 24hrs',
+      field: 'last24',
+      cellRenderer: 'agSparklineCellRenderer',
+      cellRendererParams: {
+        sparklineOptions: {
+          line: {
+            strokeWidth: 2,
+          },
+        },
+      },
+    },
+  ];
+
+  statusBar = {
+    statusPanels: [
+      { statusPanel: 'agTotalAndFilteredRowCountComponent' },
+      { statusPanel: 'agTotalRowCountComponent' },
+      { statusPanel: 'agFilteredRowCountComponent' },
+      { statusPanel: 'agSelectedRowCountComponent' },
+      { statusPanel: 'agAggregationComponent' },
+    ],
+  };
+}
